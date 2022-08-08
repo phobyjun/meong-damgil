f67801f47a5386997a2c13c0a6b9502a8bb4cae9
phobyjun <phobyjun@gmail.com>
2022-08-08 20:25:08 +0900

    FIX: Rename RepositoryTests to AccountRepositoryTests

--- src/test/kotlin/io/dasom/meongdamgil/repository/AccountRepositoryTests.kt
+++ src/test/kotlin/io/dasom/meongdamgil/repository/AccountRepositoryTests.kt
@@ -8,7 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class RepositoryTests(
+class AccountRepositoryTests(
     val accountRepository: AccountRepository
 ) : BehaviorSpec({
 
